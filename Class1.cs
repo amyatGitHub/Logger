48374e305b2d76a551c602c5e875329bbbdabebf
amyatGitHub <wyauamy@gmail.com>
2019-01-21 10:48:33 +0800

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -26,7 +26,7 @@ namespace Logger
              // Get stack trace for the exception with source file information
             var st = new System.Diagnostics.StackTrace(ex, true);
             // Get the top stack frame
-            var frame = st.GetFrame(0);
+            var frame = st.GetFrame(st.FrameCount - 1);
             // Get the line number from the stack frame
             var line = frame.GetFileLineNumber();
 
